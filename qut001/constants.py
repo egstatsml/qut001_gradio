4f6aa1c322b3dc4fab0e5f1d42a6c6f6ba0e64e7
Ethan Goan <ej.goan@qut.edu.au>
2026-09-03 11:18:33 +1000

    dev(data): adding images directory

--- qut001/constants.py
+++ qut001/constants.py
@@ -1,3 +1,5 @@
 import os
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODEL_WEIGHTS_DIR = os.getenv("MODEL_WEIGHTS_DIR") or os.path.join(ROOT_DIR, "model_weights")
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+IMAGE_DIR = os.path.join(DATA_DIR, "images") 
